293be92c88e8631ea56ac82a5d8131f992f169e9
ugur.akpinar <mehmet.akpinar@dff-tech.com>
2026-09-03 10:48:01 +0300

    add gasEngine and electricEngine types with milesLeft method and canMakeIt function for distance calculation

--- gasEngine/gasEngine.go
+++ gasEngine/gasEngine.go
@@ -12,12 +12,21 @@ type electricEngine struct {
 	kwh   int
 }
 
+type hydrogenEngine struct {
+	mpkg int
+	kg   int
+}
+
 func (e gasEngine) milesLeft() int {
 	return e.gallons * e.mpg
 }
 
 func (e electricEngine) milesLeft() int {
 	return e.kwh * e.mpkwh
+}
+
+func (e hydrogenEngine) milesLeft() int {
+	return e.kg * e.mpkg
 }
 
 type engine interface {
@@ -35,8 +44,10 @@ func canMakeIt(e engine, miles int) {
 func main() {
 	myEngine := gasEngine{25, 15}
 	myEngineElectric := electricEngine{25, 150}
+	myHydrogenEngine := hydrogenEngine{25, 150}
 	fmt.Printf("Total miles left in tank: %v\n", myEngine.milesLeft())
 	fmt.Printf("Total miles left in battery: %v\n", myEngineElectric.milesLeft())
 	canMakeIt(myEngine, 100)
 	canMakeIt(myEngineElectric, 3300)
+	canMakeIt(myHydrogenEngine, 3300)
 }
